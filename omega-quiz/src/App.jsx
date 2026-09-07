884e3889ae7c5f46f782e3dedbfd06f5d2bf7676
NoelFockstedt <noel.fockstedt@omegapoint.se>
2026-09-07 09:41:10 +0200

    project init

---
+++ omega-quiz/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [questions, setQuestions] = useState([])
+
+  return (
+    <>
+      <h1>Omega Quiz</h1>
+      <p>Welcome to the Omega Quiz!</p>
+
+    </>
+  )
+}
+
+export default App
