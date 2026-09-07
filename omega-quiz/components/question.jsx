eb67d4f88812bfdccbcf570a44f108da136a8cc6
Hjalmar Dahlberg <hjalmar.dahlberg@gmail.com>
2026-09-07 13:26:26 +0200

    escape html characters in question

--- omega-quiz/components/question.jsx
+++ omega-quiz/components/question.jsx
@@ -1,15 +1,41 @@
 import { useState } from 'react'
 
 function question({click, category, questionText, correctAnswer, incorrectAnswers}) {
+
+    const decodeHTMLEntities = (str) => {
+    if(str && typeof str === 'string') {
+      // strip script/html tags
+      str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
+      str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
+      element.innerHTML = str;
+      str = element.textContent;
+      element.textContent = '';
+    }
+
+
+
+
     var allAnswers = [...incorrectAnswers, correctAnswer];
+
+    for (let index = 0; index < allAnswers.length; index++) {
+        allAnswers[index]=decodeHTMLEntities(allAnswers[index]);
+        
+    }
+
     const right_answer = correctAnswer;
+
+
     allAnswers.sort(() => Math.random() - 0.5);
 
+    
+
+    return str;
+  }
 
     return (
     <>
-        <h2>{category}</h2>
-        <p>{questionText}</p>
+        <h2>{decodeHTMLEntities(category)}</h2>
+        <p>{decodeHTMLEntities(questionText)}</p>
         <ul>
             {allAnswers.map((answer,index) => (<button key={index} onClick={() => click(answer, right_answer)}>{answer}</button>))}
         </ul>
